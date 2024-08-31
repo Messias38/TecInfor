0b10b7b8b48bbfc3fbd47f52ed6ac66b7961a769
Messias38 <messiasserram@gmail.com>
2024-08-30 21:27:18 -0300

    Add files via upload

---
+++ atividade-array.php
@@ -0,0 +1,11 @@
+<?php
+$estado = array(
+    "SP" => "São Paulo",
+    "RJ" => "Rio de Janeiro",
+    "MG" => "Belo Horizonte",
+    "RS" => "Porto Alegre",
+    "BA" => "Salvador"
+);
+
+var_dump($estado);
+?>
